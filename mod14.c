e62cc532dc631d602c6a6f9c8e482fa01625c958
Timothy Meade <zt.tmzt@gmail.com>
2010-10-10 23:17:48 -0400

    add name to symlink debug

--- mod14.c
+++ mod14.c
@@ -63,7 +63,7 @@ static void walk_dir(struct sysfs_dirent *dir, char *name, int linkdepth, void (
 	};
 
 	if (dir->s_flags & SYSFS_KOBJ_LINK) {
-		printk("linkdepth: %d\n", linkdepth);
+		printk("name: %s linkdepth: %d\n", name, linkdepth);
 		if (linkdepth > 0) {
 			printk("following symlink: %s\n", dir->s_symlink.target_sd->s_name);
 			walk_dir(dir->s_symlink.target_sd, name, linkdepth-1, found_it);
