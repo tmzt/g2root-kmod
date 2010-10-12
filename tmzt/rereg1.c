c02902e0d7914ba1d8810ca111144f9130416f36
Timothy Meade <zt.tmzt@gmail.com>
2010-10-12 17:28:00 -0400

    adding unreg1, compile with DOSTUFF=1 on the make line to actaully remove the device, also readding rereg1 to tmzt

---
+++ tmzt/rereg1.c
@@ -0,0 +1,67 @@
+#include <linux/module.h>
+#include <linux/init.h>
+
+#include <linux/platform_device.h>
+
+#include "../arch/arm/mach-msm/include/mach/msm_iomap.h"
+
+static struct resource resources_sdc2[] = {
+	{
+		.start	= MSM_SDC2_PHYS,
+		.end	= MSM_SDC2_PHYS + MSM_SDC2_SIZE - 1,
+		.flags	= IORESOURCE_MEM,
+	},
+	{
+		.start	= INT_SDC2_0,
+		.end	= INT_SDC2_0,
+		.flags	= IORESOURCE_IRQ,
+		.name	= "cmd_irq",
+	},
+		{
+		.start	= INT_SDC2_1,
+		.end	= INT_SDC2_1,
+		.flags	= IORESOURCE_IRQ,
+		.name	= "pio_irq",
+	},
+	{
+		.flags	= IORESOURCE_IRQ | IORESOURCE_DISABLED,
+		.name	= "status_irq"
+	},
+	{
+		.start	= 8,
+		.end	= 8,
+		.flags	= IORESOURCE_DMA,
+	},
+};
+
+struct platform_device msm_device_sdc2 = {
+        .name           = "msm_sdcc",
+        .id             = 2,
+        .num_resources  = ARRAY_SIZE(resources_sdc2),
+        .resource       = resources_sdc2,
+        .dev            = {
+                .coherent_dma_mask      = 0xffffffff,
+        },
+};
+
+static void rereg() {
+    printk("registering platform device...\n");
+    platform_device_register(&msm_device_sdc2);
+    printk("done\n");
+}
+
+
+static int __init test_init(void) {
+        printk("test module loaded\n");
+        rereg();
+	return 0;
+}
+
+static void __exit test_exit(void) {
+        printk("test module unloaded\n");
+}
+
+module_init(test_init);
+module_exit(test_exit);
+
+MODULE_LICENSE("GPL");
---
+++ tmzt/unreg1.c
@@ -0,0 +1,86 @@
+#include <linux/module.h>
+#include <linux/init.h>
+
+#include <linux/version.h>
+
+#include <linux/mmc/host.h>
+#include <linux/mmc/card.h>
+//#include "../drivers/mmc/host/msm_sdcc.h"
+
+#include <linux/kdev_t.h>
+#include <linux/fs.h>
+#include <linux/genhd.h>
+
+#if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,32))
+#define disk_to_dev(disk)       (&(disk)->part[0]->dev)
+#else
+#define disk_to_dev(disk)       (&(disk)->part0.__dev)
+#endif
+
+static void host_off(struct mmc_host *host) {
+    #ifdef DOSTUFF
+    printk("removing mmc_host: %s\n", mmc_hostname(host));
+    mmc_remove_host(host);
+    #else
+    #warning "nop'd version"
+    printk("not turning off: %s\n", mmc_hostname(host));
+    #endif
+    return;
+}
+
+static struct mmc_host *get_host(const char* path) {
+    struct block_device *bd = lookup_bdev(path);
+    if (IS_ERR(bd)) {
+        printk("%s: invalid block device (are you on android/mdev?)\n", path);
+        return -ENODEV;
+    }
+    printk("%s: block_device: %.8x\n", path, (unsigned int)bd);
+    struct gendisk *gd = bd->bd_disk;
+    printk("%s: gendisk: %.8x\n", path, (unsigned int)gd);
+    #if (LINUX_VERSION_CODE < KERNEL_VERSION(2,6,32))
+    struct device *part_dev = disk_to_dev(gd);
+    printk("%s: (part) device: %.8x\n", path, (unsigned int)part_dev);
+    printk("%s: (part) device name: %s\n", path, dev_name(part_dev));
+    struct device *disk_dev = part_dev->parent;
+    #else
+    struct device *disk_dev = disk_to_dev(gd);
+    #endif
+    printk("%s: (disk) device: %.8x\n", path, (unsigned int)disk_dev);
+    printk("%s: (disk) device name: %s\n", path, dev_name(disk_dev));
+    struct device *card_dev = disk_dev->parent;
+    printk("%s: (card) device: %.8x\n", path, (unsigned int)card_dev);
+    printk("%s: (card) device name: %s\n", path, dev_name(card_dev));
+    struct mmc_card *card = container_of(card_dev, struct mmc_card, dev);
+    printk("%s: card: %.8x\n", path, (unsigned int)card);
+    struct mmc_host *host = card->host;
+    printk("%s: host: %.8x\n", path, (unsigned int)host);
+    return host;
+
+//  struct mmc_card *card = bdget(MKDEV(179, 0))->bd_disk->part0->__dev.parent;
+//  struct mmc_host *host = container_of(card, struct mmc_card, dev)->host;
+//  struct mmc_host *host = container_of(card, struct mmc_card, dev)->host;
+}
+
+static int test_card(const char* path) {
+    struct mmc_host *host = get_host(path);
+    if (IS_ERR(host)) return -ENODEV;
+    host_off(host);
+    return 0;
+}
+
+static int __init test_init(void) {
+        printk("test module loaded\n");
+
+        int res = test_card("/dev/mmcblk0");
+        if (res < 0) res = test_card("/dev/block/mmcblk0");
+    	return 0;
+}
+
+static void __exit test_exit(void) {
+        printk("test module unloaded\n");
+}
+
+module_init(test_init);
+module_exit(test_exit);
+
+MODULE_LICENSE("GPL");
