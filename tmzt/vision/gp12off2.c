7443fdd7fa5d1969b43f848084427198483ba637
Timothy Meade <zt.tmzt@gmail.com>
2010-10-15 05:53:38 -0400

    use ksym address for msm_proc_comm gp12off2

---
+++ tmzt/vision/gp12off2.c
@@ -0,0 +1,33 @@
+#include <linux/module.h>
+#include <linux/init.h>
+#include <linux/device.h>
+
+#include <mach/vreg.h>
+
+#include "../arch/arm/mach-msm/proc_comm.h"
+
+int (*stupid_proc_comm)(uint, uint*, uint*) = (void *)0xc003bdbc;
+
+static int __init test_init(void) {
+    int res;
+    int id;
+    int enable;
+
+    printk("test module loaded\n");
+    printk("turning off GP12 (#37)\n");
+    id = 37;
+    enable = 0;
+    res = stupid_proc_comm(PCOM_VREG_SWITCH, &id, &enable);
+    printk("result: %d\n", res);
+    printk("done.\n");
+	return 0;
+}
+
+static void __exit test_exit(void) {
+        printk("test module unloaded\n");
+}
+
+module_init(test_init);
+module_exit(test_exit);
+
+MODULE_LICENSE("GPL");
